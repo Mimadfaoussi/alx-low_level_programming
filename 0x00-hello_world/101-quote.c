fb60711bcdd15d6da3951e1fc51bd58ab6414fad
Mimadfaoussi <imadfaoussi@gmail.com>
2023-09-08 02:29:58 +0100

    write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ *main - print a text
+ *Return:1 for an error !
+ */
+
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	write(1, quote, sizeof(quote));
+	return (1);
+}
