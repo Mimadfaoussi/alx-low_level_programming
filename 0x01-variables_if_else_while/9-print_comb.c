f41fbcbb53820b7d16ca4428d74e41e47ee4cefa
Mimadfaoussi <imadfaoussi@gmail.com>
2023-09-09 00:42:57 +0100

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,13 +10,16 @@ int main(void)
 	int alpha;
 
 	for (alpha = 48; alpha <= 57; alpha++)
+	{
+
 		putchar(alpha);
 		if (alpha != 57)
 		{
 			putchar(',');
 			putchar(' ');
 		}
-	putchar('\n');
+	}
+		putchar('\n');
 	return (0);
 }
 
