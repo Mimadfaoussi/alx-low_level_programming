35164e307250b5f17480333581e7db726788f39a
Mimadfaoussi <imadfaoussi@gmail.com>
2023-09-09 00:21:38 +0100

    alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - just a main that print alphabets
+ * Return:0 for success
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha != 'q' && alpha != 'e')
+			putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
+
