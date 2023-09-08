bfad27a96ee93f2e4583c682a627bf6300f55bc3
Mimadfaoussi <imadfaoussi@gmail.com>
2023-09-08 02:44:18 +0100

    write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,6 @@
 
 int main(void)
 {
-	char qte[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(1, qte, sizeof(qte));	
-	write(1, "\n", 1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
