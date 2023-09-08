6bbafa5cfe8f31a07cb582fdefd6ec4d7589ab0b
Mimadfaoussi <imadfaoussi@gmail.com>
2023-09-08 01:10:39 +0100

    hello,puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+/*
+ * File : 5-printf.c
+ * Author : Imad faoussi <imadfaoussi@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - this is a main file
+ *
+ * Return:always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
