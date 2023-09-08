44cd4eabdbee5cda7fc9781fa0e60a557b03ed3b
Mimadfaoussi <imadfaoussi@gmail.com>
2023-09-08 01:04:35 +0100

    hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-	/**
-	* main just a description forwq
-	*the main function
-	*/
-int main(void)
+/**
+ * main   -  Prints a text only
+ *
+ * Return: Always 0.
+ */
 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
