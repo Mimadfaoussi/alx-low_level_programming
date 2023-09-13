8fcfbacdabc82ebaf78a400b34604e406227bd49
Mimadfaoussi <imadfaoussi@gmail.com>
2023-09-13 23:16:34 +0100

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,10 +10,10 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
+	if (n == -2147483648)
+		n++;
 	if (n < 0)
 		n = -n;
-	if (n == 2147483648)
-		n--;
 	last_digit = n % 10;
 	_putchar(last_digit + '0');
 	return (last_digit);
