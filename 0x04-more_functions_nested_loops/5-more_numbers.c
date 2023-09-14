a711652b7900a5960e5f53d5184958bfcc25cb5e
Mimadfaoussi <imadfaoussi@gmail.com>
2023-09-14 17:09:43 +0100

    print_numbers until 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,11 +13,12 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (b >= 10)
+			if (j >= 10)
 				_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
 		}
-		if (i != 9 )
+		if (i != 9)
 			_putchar('\n');
 	}
 }
+
