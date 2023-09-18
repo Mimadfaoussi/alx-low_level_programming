34d2fe9fb7344e5d46f010d567c1e1c179b183e9
Mimadfaoussi <imadfaoussi@gmail.com>
2023-09-18 21:55:51 +0100

    puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -33,7 +33,8 @@ void puts_half(char *str)
 		half = (length - 1) / 2;
 	else
 		half = length / 2;
-	half++;
+	if (length % 2 != 0)
+		half++;
 	while (str[half] != '\0')
 	{
 		_putchar(str[half]);
