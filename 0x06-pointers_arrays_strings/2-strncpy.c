011725774a24a328b36aa7ada6fa90260c1429e9
Mimadfaoussi <imadfaoussi@gmail.com>
2023-09-21 02:37:49 +0100

    _strncat

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy-copy n bytes from src to dest.
+ * @dest:destination string.
+ * @src:source string to copy from.
+ * @n:how much to copy.
+ * Return:dest string.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while ((i < n) && (src[i] != '\0'))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
