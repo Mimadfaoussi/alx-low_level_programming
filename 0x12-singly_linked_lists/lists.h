e08a355673a4e898ba614843c76c87cf6e61a9a8
Mimadfaoussi <imadfaoussi@gmail.com>
2023-10-19 05:54:34 -0700

    testing

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,3 +1,5 @@
+#ifndef _CODING_H_
+#define _CODING_H_
 #include <stdio.h>
 /**
  * struct list_s - singly linked list
@@ -16,4 +18,10 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
+char *_strdup(const char *str);
+#endif
