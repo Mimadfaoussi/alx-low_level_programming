0fbbc84143dcd68d7b0f9c0e96d7f5e6ea3ca3fc
Mimadfaoussi <imadfaoussi@gmail.com>
2023-10-19 06:56:48 -0700

    adding my own strdup

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,39 @@
 #include <stdlib.h>
 #include "lists.h"
+/**
+ * *_strdup - function with one argument
+ * @str: string argument
+ *
+ * Description: returns a pointer to allocated space in memory
+ * Return: pointer
+ */
+char *_strdup(const char *str)
+{
+	int i, j;
+	char *ptr;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+
+	ptr = malloc(sizeof(char) * i + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	j = 0;
+	while (str[j] != '\0')
+	{
+		ptr[j] = str[j];
+		j++;
+	}
+	ptr[j] = '\0';
+	return (ptr);
+}
 
 /**
  * add_node - function with two arguments
@@ -16,7 +50,7 @@ list_t *add_node(list_t **head, const char *str)
 	ptr = malloc(sizeof(list_t));
 	if (ptr == NULL)
 		return (NULL);
-	ptr->str = strdup(str);
+	ptr->str = _strdup(str);
 	while (str[count] != '\0')
 		count++;
 	ptr->len = count;
