de6800ad03388735d8c6719fa28c9f2de9b799ee
Mimadfaoussi <imadfaoussi@gmail.com>
2023-10-19 06:21:35 -0700

    correcting the incompatible assignment error

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-
+#include <stdlib.h>
 /**
  * list_len - function with one argument that cout nb of elmts
  * @h: const list_t pointer argument to struct.
@@ -8,7 +8,7 @@
 size_t list_len(const list_t *h)
 {
 	size_t	count;
-	list_t	ptr;
+	list_t	*ptr;
 
 	count = 0;
 	ptr = (list_t *)h;
