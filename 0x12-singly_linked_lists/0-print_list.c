ab81c519f1ce3d4b23a978fc5bbcd659027a8f21
Mimadfaoussi <imadfaoussi@gmail.com>
2023-10-19 05:44:39 -0700

    adding files for linked lists project

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * print_list-function that print list elmnts.
+ * @h:list to print.
+ * Return:nb of nodes.
+ * Description: singly linked list node structure
+ */
+
+size_t print_list(const list_t *h)
+{
+	list_t	*tmp;
+	size_t	count;
+
+	tmp = (list_t *)h;
+	count = 0;
+	while (tmp != NULL)
+	{
+		if (tmp->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", tmp->len, tmp->str);
+		tmp = tmp->next;
+		count++;
+	}
+	return (count);
+}
+
