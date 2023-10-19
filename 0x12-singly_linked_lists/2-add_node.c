5be7bb10d3ecf6fecd39e38865bb46d8bab807f8
Mimadfaoussi <imadfaoussi@gmail.com>
2023-10-19 06:35:13 -0700

    adding a head to the list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_node - function with two arguments
+ * @head: double pointer to linked list
+ * @str: string pointer
+ * Return:adress of the new element.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t	*ptr;
+	int	count;
+
+	count = 0;
+	ptr = malloc(sizeof(list_t));
+	if (!ptr)
+		return (NULL);
+	ptr->str = strdup(str);
+	while (str[count] != '\0')
+		count++;
+	ptr->len = count;
+	ptr->next = *head;
+	*head = ptr;
+	return (ptr);
+}
