7c99557076a460197408a00928e1b456b50039ff
Mimadfaoussi <imadfaoussi@gmail.com>
2023-10-20 02:26:25 -0700

    free alocated space

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - free a grid that was allocated by alloc_grid
+ *
+ * @grid: - grid to free
+ * @height: - height of grid
+ *
+ * Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int	i;
+
+	i = 0;
+	if (gird == NULL)
+		return;
+	if (grid[i] == NULL)
+		return;
+	while (i < height)
+	{
+		free(grid[i]);
+		i++;
+	}
+	free(grid);
+}
