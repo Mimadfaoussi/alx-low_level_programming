e8b2aa46b3ceee75c1c9d128973ecbc641f101d2
Mimadfaoussi <imadfaoussi@gmail.com>
2023-10-21 10:42:16 -0700

    print nb of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - print programmes nb of args.
+ * @argc : nb of args.
+ * @argv : pointer to list of args.
+ * Return: 0 for success and other for faillure .
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc >= 1)
+		printf("%d\n", argc);
+	else
+		printf("error");
+	return (0);
+}
+
+
