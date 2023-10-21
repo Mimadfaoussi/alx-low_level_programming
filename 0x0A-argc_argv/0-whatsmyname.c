ab96c5b4c58bc5dfc0c6bedfad8b9c6eae57faf2
Mimadfaoussi <imadfaoussi@gmail.com>
2023-10-21 10:39:20 -0700

    programme's name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,19 @@
 #include "main.h"
 
+/**
+ * main - print programmes name.
+ * @argc : nb of args.
+ * @argv : pointer to list of args.
+ * Return: 0 for success and other for faillure .
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc >= 1)
+		printf("%s\n", argv[0]);
+	else
+		printf("error");
+	return (0);
+}
+
 
