99dde122119de00e7f663fed7687055dfcb9aaf9
Mimadfaoussi <imadfaoussi@gmail.com>
2023-10-22 23:39:18 -0700

    nb nodes and print

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints all elements of a list
+ * @h: Pointer to a list
+ * Return: Number of nodes.
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	listint_t	*ptr;
+	size_t		nbnodes;
+
+	nbnodes = 0;
+	ptr = (listint_t *)h;
+	while (ptr != NULL)
+	{
+		printf("%d\n", ptr->n);
+		nbnodes++;
+		ptr = ptr->next;
+	}
+	return (nbnodes);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+README ABOUT : advanced linked lists.
