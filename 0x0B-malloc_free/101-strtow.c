51851628607cc6188505f06444d69bab38d4650e
Mimadfaoussi <imadfaoussi@gmail.com>
2023-10-24 10:15:45 -0700

    back version

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,68 +1,83 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-/**
- * count_word - helper function to count the number of words in a string
- * @s: string to evaluate
- * Return: number of words
-*/
-int count_word(char *s)
-{
-	int 	b;
-        int	i;
-	int	word;
 
-	b = 0;
-	word = 0;
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == ' ')
-			b = 0;
-		else if (b == 0)
-		{
-			b = 1;
-			word++;
-		}
-	return (word);
-}
-/**	
- * strtow - splits a string into words
- * @str: string to split
- * Return: pointer to an array of strings (Success)
- * or NULL (Error)		
- */		
-char **strtow(char *str)		
+
+/**
+ * words - function that splits string into words
+ * @str: string being passed
+ * Return: a pointer to an array of strings (words).
+ */
+int	words(char	*str)
 {
-	char **matrix, *tmp;
-	int i, k = 0, len = 0, words, c = 0, start, end;
-	while (*(str + len))
-		len++;
-	words = count_word(str);
-	if (words == 0)
-		return (NULL);
-	matrix = (char **) malloc(sizeof(char *) * (words + 1));
-	if (matrix == NULL)
-		return (NULL);
-	for (i = 0; i <= len; i++)
+	int	i;
+	int	nb_words;
+
+	nb_words = 0;
+	i = 0;
+	while (str[i] != '\0')
 	{
-		if (str[i] == ' ' || str[i] == '\0')	
-		{
-			if (c)
-			{
-				end = i;
-				tmp = (char *) malloc(sizeof(char) * (c + 1));
-				if (tmp == NULL)
-					return (NULL);
-				while (start < end)
-					*tmp++ = str[start++];
-				*tmp = '\0';
-				matrix[k] = tmp - c;
-				k++;
-				c = 0;
-			}
-		}
-		else if (c++ == 0)
-			start = i;
-	}	
-	matrix[k] = NULL;
-	return (matrix);
+		if (str[i] == ' ')
+			nb_words++;
+		i++;
+	}
+	return (nb_words);
 }
+
+/**
+ * strtow - function that splits string into words
+ * @str: string being passed
+ * Return: a pointer to an array of strings (words).
+ */
+char **strtow(char *str)
+{
+	char	**grid;
+	int	nb_words;
+	int	i;
+	int	j;
+	int	size;
+	int	index;
+	int	k;
+
+	if (str == NULL || str == "")
+		return (NULL);
+	nb_words = words(str);
+	grid = malloc(sizeof(char *) * nb_words + 1);
+	if (grid == NULL)
+		return (NULL);
+	i = 0;
+	j = 0;
+	index = 0;
+	while (i < nb_words)
+	{
+		size = 0;
+		j = index;
+		while (str[index] != '\0' && str[index] != ' ')
+		{
+			index++;
+			size++;
+		}
+		index++;
+		size++;
+		grid[i] = malloc(sizeof(char) * size);
+		if (grid[i] == NULL)
+		{
+			while (i >= 0)
+				free(grid[i]);
+			free(grid);
+			return (NULL);
+		}
+		k = 0;
+		while (j < index - 1)
+		{
+			grid[i][k] = str[j];
+			j++;
+			k++;
+		}
+		grid[i][k] = '\0';
+		i++;
+	}
+	grid[i] = NULL;
+	return (grid);
+}
+
