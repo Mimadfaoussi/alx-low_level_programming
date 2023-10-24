5e2a414012b3a6f8ef46fa0d405d9a5a806fb16a
Mimadfaoussi <imadfaoussi@gmail.com>
2023-10-24 01:25:14 -0700

    reversing list with restrictions

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - Reverse a given list
+ * @head: Pointer to a pointer to the list.
+ * Return: Address of head.
+ */
+
+listint_t	*reverse_listint(listint_t **head)
+{
+	listint_t	*ptr;
+	listint_t	*index;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+	index = (*head)->next;
+	ptr = index->next;
+	(*head)->next = NULL;
+	index->next = *head;
+	while (ptr != NULL)
+	{
+		*head = index;
+		index = ptr;
+		ptr = ptr->next;
+		index->next = *head;
+	}
+	*head = index;
+	return (*head);
+}
