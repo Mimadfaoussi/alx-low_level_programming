070601aecd7b91157bf4c6d6ddfc585866f60b6e
Mimadfaoussi <imadfaoussi@gmail.com>
2023-10-26 15:20:09 -0700

    converting binary to int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,62 @@
+/**
+ * binary_to_uint - convert binary string to decimal
+ * @b: binary string
+ *
+ * Return: decimal (unsigned int)
+ */
+#include <stdio.h>
+
+unsigned int _power(unsigned int nb, unsigned int times)
+{
+	unsigned int i;
+	unsigned int x;
+
+	i = 0;
+	x = 1;
+
+	if (times == 0)
+		return (1);
+	while (i < times)
+	{
+		x = x * nb;
+		i++;
+	}
+	return (x);
+}
+unsigned int _strlen(const char *b)
+{
+	unsigned int i;
+
+	i = 0;
+	if (b == NULL)
+		return (i);
+	while (b[i] != '\0')
+		i++;
+	return (i);
+}
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int nb;
+	unsigned int length;
+	unsigned int i;
+
+	if (b == NULL)
+		return (0);
+	i = 0;
+	length = _strlen(b) - 1;
+	nb = 0;
+	while (length > 0)
+	{
+		if (b[length] != '0' && b[length] != '1')
+			return (0);
+		if (b[length] == '1')
+			nb = nb + _power(2, i);
+		i++;
+		length--;
+	}
+	if (b[length] == '1')
+		nb = nb + _power(2, i);
+	return (nb);
+}
+
