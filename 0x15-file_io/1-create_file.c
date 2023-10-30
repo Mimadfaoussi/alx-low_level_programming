4dbb5d5ad6f9bb5183a308ce012a14d031a4ed7b
Mimadfaoussi <imadfaoussi@gmail.com>
2023-10-30 11:19:49 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename:the name of the file to create
+ * @text_content:is a NULL terminated string to write to the file
+ * Return:1 on success , -1 on failure.
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int	f;
+	int	i;
+	int	j;
+
+	if (filename == NULL)
+		return (-1);
+	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (f == -1)
+		return (-1);
+	i = 0;
+	j = 0;
+	while (text_content[i] != '\0')
+		i++;
+	if (i > 0)
+		i++;
+	j = write(fd, text_content, i);
+	if (j == -1)
+	{
+		close(f)
+		return (-1);
+	}
+	close(f);
+	return (j);
+}
