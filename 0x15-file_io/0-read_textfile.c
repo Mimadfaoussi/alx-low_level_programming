6dd68c9a01a088b78a00b02e3d6c08d50930bd9b
Mimadfaoussi <imadfaoussi@gmail.com>
2023-10-30 08:34:16 -0700

    printing file content into stdout

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#incldue "main.h"
+
+/**
+ * read_textfile - reads atext file and prints it to the POSIX standard output
+ * @filename:files name.
+ * letters:the number of letters it should read and print.
+ * Return:the actual number of letters it could read and print.
+ * otherwise 0.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	File	*f;
+	int	size;
+	char	*buffer;
+	int	length;
+
+	if (filename == NULL || letters == 0)
+		return (0);
+	buffer = (char *)malloc(sizeof(char) * letters);
+	if (!buffer)
+		return (0);
+	f = open(filename, O_RDONLY);
+	if (f == NULL)
+	{
+		free(buffer);
+		return (0);
+	}
+	size = read(f, buffer, letters);
+	if (size == -1)
+	{
+		free(buffer);
+		close(f);
+		return (0);
+	}
+	length = write(STDOUT_FILENO, buffer, size);
+	free(buffer);
+	close(f);
+	if (length != size)
+		return (0);
+	return (size);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,17 @@
+#ifndef CODE_H
+# define CODE_H
+
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <ctype.h>
+# include <unistd.h>
+
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+
+
+#endif
