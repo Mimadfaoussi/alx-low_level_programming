87b531efc8e6f4481a0230b3aba3a7229bc89c5f
Mimadfaoussi <imadfaoussi@gmail.com>
2023-11-03 14:51:26 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,8 +20,11 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	i = 0;
 	j = 0;
-	while (text_content[i] != '\0')
-		i++;
+	if (text_content != NULL)
+	{
+		while (text_content[i] != '\0')
+			i++;
+	}
 	j = write(f, text_content, i);
 	if (j == -1)
 	{
