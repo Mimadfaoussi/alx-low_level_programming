d8bff2eb5d7c736eb911461d4bee590a8456efc6
imad faoussi <imadfaoussi@gmail.com>
2024-02-08 10:12:45 -0800

    print number in binary using bitwise operations

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_binary-prints the binary representation of a number
+ * @n:number to print as binary
+ * Return:nothing it's a void funciton
+ */
+
+void print_binary(unsigned long int n)
+{
+	int			index;
+	int			j;
+	unsigned long int	number;
+
+	j = 0;
+	index = 63;
+	while (index)
+	{
+		number = n >> index;
+		if (number & 1)
+		{
+			_putchar('1');
+			j = 1;
+		}
+		else
+		{
+			if (j == 1)
+				_putchar('0');
+		}
+		index--;
+	}
+	if (j == 0)
+		_putchar('0');
+}
---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _putchar- print char to screen
+ * @c: char to print
+ * Return : nothing
+ */
+
+void _putchar(int c)
+{
+	write(1, &c, 1);
+}
